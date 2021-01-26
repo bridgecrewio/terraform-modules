cf4487ba2e351fa4b0f83270938866483ef77229
elaygl <elaygl@gmail.com>
2021-01-26 17:42:58 +0200

    Disabled redis

--- modules/aws-asg/locals.tf
+++ modules/aws-asg/locals.tf
@@ -4,7 +4,8 @@
 
 locals {
 
-  redis_enabled = var.asg_desired_capacity > 1 ? true : false
+  # redis_enabled = var.asg_desired_capacity > 1 ? true : false
+  redis_enabled = false
 
   common_tags_map = {
     application      = "fyde-access-proxy"
